7865e82eaff90cdfa56dd13909d6fff1896f3d37
Ojo Morakinyo Samuel <arayosam@gmail.com>
2024-04-16 14:41:51 +0100

    saved

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import AddBooks from '../components/AddBooks'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4500/graphql',
+  // uri: 'http://localhost:4500/graphql',
+  uri: 'https://backgraph.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
